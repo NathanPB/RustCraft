21a5b0b17067c538145616e31503bb410632842e
NathanPB <nathan.pbombana@gmail.com>
2017-08-15 22:24:31 -0300

    Added 338mm ammo (for snipers)

---
+++ src/main/java/cf/nathanpb/RustCrafto/bullets/Bullet338.java
@@ -0,0 +1,11 @@
+package cf.nathanpb.RustCrafto.bullets;
+
+/**
+ * Created by nathanpb on 8/15/17.
+ */
+public class Bullet338 extends Bullet{
+    public Bullet338(){
+        setGravity(0.02);
+        setDamage(28);
+    }
+}
