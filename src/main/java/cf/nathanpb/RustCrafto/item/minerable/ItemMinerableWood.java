3841542179014f5b1f8bd529a467fbe91ebb4577
NathanPB <nathan.pbombana@gmail.com>
2017-08-18 22:42:04 -0300

    done

--- src/main/java/cf/nathanpb/RustCrafto/item/minerable/ItemMinerableWood.java
+++ src/main/java/cf/nathanpb/RustCrafto/item/minerable/ItemMinerableWood.java
@@ -1,9 +1,17 @@
 package cf.nathanpb.RustCrafto.item.minerable;
 
+import cf.nathanpb.RustCrafto.Core;
 import cf.nathanpb.RustCrafto.item.RustCraftItem;
+import cf.nathanpb.RustCrafto.item.ores.ItemOreWoodLog;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.Random;
 
 /**
  * Created by nathanpb on 8/17/17.
@@ -13,5 +21,26 @@ public class ItemMinerableWood extends RustCraftItem implements Listener{
         super();
         material = Material.LOG;
         name = ChatColor.BLACK+"OreWood";
+    }
+    @Override
+    protected void onBlockBreak(BlockBreakEvent e) {
+        super.onBlockBreak(e);
+        Bukkit.broadcastMessage("quebrado");
+        if(!e.getBlock().hasMetadata("cooldown")) {
+            org.bukkit.inventory.ItemStack drop1 = RustCraftItem.getInstance(ItemOreWoodLog.class).get();
+            drop1.setAmount(new Random().nextInt(5));
+            e.getBlock().getWorld().dropItem(e.getBlock().getLocation(), drop1);
+            e.getBlock().setMetadata("cooldown", new FixedMetadataValue(Core.getInstance(), 20 * 60 * 10));
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    e.getBlock().removeMetadata("cooldown", Core.getInstance());
+                }
+            }.runTaskLater(Core.getInstance(), e.getBlock().getMetadata("cooldown").get(0).asInt());
+        }
+        if(!metadata.hasKey(e.getBlock().toString())) {
+            metadata.put(e.getBlock().toString(), this.getClass().getName());
+        }
+        e.setCancelled(true);
     }
 }
