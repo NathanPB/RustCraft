f18f1c79af6f305f888b871904d24724e09aa31c
NathanPB <nathan.pbombana@gmail.com>
2017-08-20 19:08:51 -0300

    Improved imports

--- src/main/java/cf/nathanpb/RustCrafto/Core.java
+++ src/main/java/cf/nathanpb/RustCrafto/Core.java
@@ -1,11 +1,15 @@
 package cf.nathanpb.RustCrafto;
 
+import cf.nathanpb.RustCrafto.Utils.PlayerUtils;
 import cf.nathanpb.RustCrafto.commands.RustCraft;
 import cf.nathanpb.RustCrafto.item.RustCraftItem;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.io.File;
+import java.util.Timer;
 
 /**
  * Created by nathanpb on 8/14/17.
@@ -21,6 +25,14 @@ public class Core extends JavaPlugin{
         if(!PlayerProfileDatabase.exists()) PlayerProfileDatabase.mkdirs();
         this.getCommand("rustcraft").setExecutor(new RustCraft());
         RustCraftItem.init();
+        new BukkitRunnable(){
+            @Override
+            public void run() {
+                for(Player p : Bukkit.getOnlinePlayers()){
+                    PlayerUtils.sendInformation(p);
+                }
+            }
+        }.runTaskTimerAsynchronously(getInstance(), 1, 0);
     }
 
     @Override
